112fbef62047f49c7d8308cef63817861d531ccd
Eyal Gerby <eyalg535@gmail.com>
2025-06-05 10:57:05 -0400

    sdfsg

--- summit-final-site/pages/index.js
+++ summit-final-site/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ServiceGallery from '/Users/eyalgerby/Desktop/Summit/Web/SUMMIT_FINAL/summit-final-site/ServiceGallery.jsx';
+import ServiceGallery from '../ServiceGallery';
 
 
 export default function Home() {
