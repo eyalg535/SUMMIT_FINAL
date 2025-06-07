7254723d6b00a7ea6a9600c83c508d09584fb80d
Eyal Gerby <eyalg535@gmail.com>
2025-06-06 20:08:26 -0400

    hfh

--- summit-final-site/ServiceGallery.jsx
+++ summit-final-site/ServiceGallery.jsx
@@ -42,10 +42,11 @@ export default function ServiceGallery({ title, images }) {
               &times;
             </button>
             <img
-              src={selectedImage}
-              alt={caption}
-              className="max-w-full max-h-[85vh] rounded-lg shadow-2xl"
-            />
+  src={selectedImage}
+  alt={caption}
+  className="w-auto h-auto max-w-[90vw] max-h-[80vh] object-contain rounded-lg shadow-2xl"
+/>
+
             <p className="text-white text-center mt-4">{caption}</p>
           </div>
         </div>
