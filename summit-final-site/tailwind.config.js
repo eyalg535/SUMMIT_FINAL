a63ecf54207931cd89ee21d5e9622c4650d7c4ba
Eyal Gerby <eyalg535@gmail.com>
2025-06-06 20:03:43 -0400

    asf

--- summit-final-site/tailwind.config.js
+++ summit-final-site/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}", // <-- add this
+    "./app/**/*.{js,ts,jsx,tsx}"         // <-- if using Next.js 13+ with app dir
+  ],
   theme: {
     extend: {},
   },
